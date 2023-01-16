8996c276b209f456e9f5f578a7b4edc0c10506c3
Xascoria <59613709+Xascoria@users.noreply.github.com>
2023-01-16 23:12:09 +0800

    Initial Commit
    I'm dead inside

---
+++ among_us.py
@@ -0,0 +1,56 @@
+                                  #############
+'''                      ''';s=["""2_2#/T0S0#-R7R.#,R9
+                    R-#*a-R,#(S3R+R,#'R6R*R,#&R7R+U(#&R7R
+                 +R'S$#&S6Q,R)Q$#'S3S,R)R#)a/R)R#(R?R)R#(R?
+               R)R#(R?R)R#(R?R)R#(R?R)R#(R?R)R#)R>R)R#)R>R(R$
+             #)R>R&S%#*R)Y.R$R(#*R+R&R.R+#*R+R&R.R+#+Q+R'R,R,#
+            +Z(R,R,#<Y.""","""2_2#/T0S0#-R7R.#+S9R-#+Q'`(R,#*V2
+           S&R,#*T6R&Q,#)T7R&S*#)T7R&Q%S'#)T6R'R%R'#)Q%S3S'R&R&#
+          )Q'a*R&R&#(R0R0R&R&#(R?R&R&#(R?R&R&#(R?R&R&#(R?R&R&#(R?
+        R&R&#)R>R&R&#)R>R%R'#)R>U(#*R*Y-R+#*R,R&R,R,#*R,R&R,R,#+R
+      *R(Q,Q-#                    ,Z)Z""","""2_2#/T0S0#-R7R.#+S9R-
+    #*R*`                                $S,#*R'S3T,#)R'R6S,#)R&R7
+  T+#)R                                    &R7T+#)R&R7T+#)R'S3S$R+#
+ )R)Z$                                      W'R*#)R3Q.R*#)R?R*#)R?R
+*#)Q                                        @R*#)R?R*#)R?Q+#)R>R+#)
+R>R+                                        #)R4Q,R+#)S-V-R+#*R-R%R,R,
+#*R-                                        R%R,Q-#+R+R'R*R-#,R*R)W/#-Y=""","""2
+_2#/                                        T0S0#-R7R.#+S9R-#*R-a,#*R*T1S+#%V*R6R*
+#$R&                                        R)R7S)#$R&R)R7R*#$R&R)R7R*#R'R*S3T*#R'
+R,W)                                       U$R*#R&S3V)R*#R'R?R*#R'R?R*#R&S?R*#R&S?R
+ *#$R&                                    R?Q+#$R&R>R+#&U>R+#)R8Q(R+#)R.W,R+#)R.R%R
+  ,R,#*R                                 -R%R,Q-#*R,R'R*R-#+R+R)W/#-Y=""","""/`4#-R
+   3S1#,R7R                           /#+R9S-#*R4Y-#*R2R+R,#)R2R,R,#%]*R.R+#%Q.R*Q.R
+     +#$R/R)R-R+#$R0            R)R,S*#$R0R+\*#$R0R5R*#$R0R5R*#$R0R5R*#$R0Q6R*#$R/R5
+     S*#$R/R5S*#$R.R6S*#%R,R7S*#'Y*Q1R+#)R.V-R+#*R-R%R-R+#*R,R'R,R+#+R+R'R+S+#,Z(T'T
+    ,#>V/""", """/`4#-S2S1#,R7R/#+R9R.#*R:S-#*R8R%R,#)R8R&R,#(a*R&S+#(R2R(R'R+#'R4R'
+    R'R+#'R4R(S%S*#'R5Q*V*#'R5R-R*#'R5R-R*#'R5R-R*#'R5R-R*#'R4R.R*#'R4R-S*#'R3R.S*#(
+    S-U/S*#)\4R+#)R.R$S-R+#)R-R'R,R+#)S,R'R,R+#*R,R'R+S+#+[)[,#/U?""","""/`4#-S2S1#,
+    R7R/#+R9R.#*R;R-#*R;S,#)R([$U'R,#)R'R3R%S+#)R&R4R&R+#)R&R5R%R+#)R&R5R%S*#)R'Q5R&
+    R*#)Q(R4R&R*#)Q(R5R%R*#)Q(R5R%R*#)R'R5R$S*#)R'R5R%R*#)R(R3R%S*#)R(S1S%S*#)R+])S*
+    #)R>R+#)R.V-R+#)R-R'R,R+#)S,R'R,R+#*R,R'R+S+#+[)[,#/U?""","""3_1#0T0S/#.R7R-#-R9
+    R,#,R:S+#+R$R9R+#+R%R8R+#*R'R*`*#*R'R(S1R)#*R&R(R3R)#)S%R)R4R(#)V)R5R(#)R-R5R(#)
+    R-R5R(#)R-R5R(#)R-R5R(#)R-S4R(#)R.R4R(#)R/R2R)#)R0S/S)#)R3^*#)R.V.R*#)S,R'R-R*#*
+    R,R'R,R+#*R,R'R,R+#+T&T)R(T,#/T-W/""",""""2`1#/S3R/#-R8R-#,R:R,#+Z3R,#*R+S2R+#*R
+    -R1R+#*R-R+\'#)S-R)R.R&#)R-R)R/R&#)R,R*R/R&#)\+Q0R&#)R5Q1Q&#)R5R0R%#)R5Q1R%#)R5Q
+    1R%#)R5R0Q&#)R5R/R&#)R6R.R&#)R7R,R'#)R1Q*Y)#)R.V-R+#)S,R&R-R+#)S,R&R,R,#*R,R&S*S
+    ,#*T(S(Z.#.U@"""];exec('for p in range(99**9):print("".join((j:=ord(i)-35,["#"'+
+     '*(j-45)," "*j,"\\n"][(j==0)+(j <= 45)])[1]for i in s[p%9]if i>="#"));__impor'+
+     't__("time").sleep(0.3);__import__("os").system("cls"if __import__("os").name'+
+     '=="nt"else"clear")')#R4R(S%S*#'R5Q*V*#'R5R-R*#'R5R-R*#'R5R-R*#'R5R-R*#'R4R.R4R
+      #R9R.#*R;R-#*R;S,#)R([$U'R,#)R'R3R%SR9R.#*R;R-#*R;S,#)R([$U'R,#)R'R3R%S+#)R&R4
+      #R'R?R*#R'R?R*#R&S?R*#R&S#*R;R-#*R;S,#)R([$U'R,#)R'R3R%#R'R?R*#R'R?R*#R&S?R*#
+      #)R.R&#)R-R)R/R&#)R,R*R/R&#)\+Q0R&#)R5Q1Q&#)R5R0R)R.R&#)R-R)R/R&#)R,R*R/R&#R
+      #S+#+R$R9R+#+R%R8R+#*R'R*`*R;R-#*R;S,#)R([$U'R,#)R'R3R-R+#*R,R'R,R+#+R+R)R
+       #$R&R)R7R*#$R&R)R7R*#R'R+Q0R&#)R5Q1Q&#)R5R0R)R.R&#)R'R3R%SR9R#
+       #R-#*R;S,#)R([$U'R,#)R'R       3R%#R'R?R*#R'R?R*R;S,#)R([$U'R
+        #+R$R9R+#+R%R8R+#*R'R*`*      #*R'R(S1R)#*R&R(R3R([$U'R,#)R
+        #R$R9R+#+R,#)R'R3R%R8R+#       *R'R*`*R;R-#*R;S,#)R([$U'R,#
+        #)\+Q0R&#)R5Q1Q&#)R5R0R        %#)R5Q1Q&#)R5R05Q1R5Q1Q&#)R5
+        #'R,R+#)S,R'R,R+#*R,R'R        +S+#+[)[,#/U?'R5R-R*#'R5R-R*
+         #R+#)R-R'R,R+#)S,R'R,R        +#*R;R-#*R;S,#)R([$U'R,#)R'R
+         #+#*R,R'R,R+#+T&)R-R'         R,R+#)R+#+T&)R-R5Q1Q&#)R5R*R
+          #)R'R3R%SR9R.#*R9R+           #+R%R3R%SR9R.#*SR;R-#*R;SR
+              #Q0R&#)R5Q+                R%R8R+#*R'R*`*1Q&#)R5R0R
+                                           #&#)R,R*R/R&#)RR;S,#
+                                                #R'R(S1R)/
